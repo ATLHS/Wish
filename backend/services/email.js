0cedb72b6108b6e4a7463de7e2270aff2c5333a7
ATLH <s-attilah@hotmail.com>
2021-11-19 01:37:54 +0100

    feat: configuring SMTP HOST env variable

--- backend/services/email.js
+++ backend/services/email.js
@@ -3,7 +3,7 @@ const hbs = require("nodemailer-express-handlebars");
 const path = require("path");
 const transporter = nodemailer.createTransport({
   port: 587,
-  host: "smtp-mail.outlook.com",
+  host: process.env.SMTP_HOST,
   auth: {
     user: process.env.SMTP_USER_EMAIL,
     pass: process.env.SMTP_USER_PASSWORD,
